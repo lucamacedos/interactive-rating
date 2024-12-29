48b3dbbbaf2299dac66984c865aace6c9f083ea1
Lucas Macedo <lucas-macedo-2009@hotmail.com>
2024-12-29 16:11:43 -0300

    Add error message if not selected.
    Add an error message if the number is not selected.

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 const ratingNumber = document.querySelectorAll(".rating-number");
-let selectNumber = "";
+const numberError = document.querySelector(".number-error");
+const button = document.querySelector("#button");
+const containerRating = document.querySelector("#container-rating");
+const containerThank = document.querySelector("#container-thank");
+const cardSelected = document.querySelector("#card-selected");
 
+let selectedNumber = "";
 
+function selectNumber() {
+  ratingNumber.forEach(number => {
 
-ratingNumber.forEach(number => {
+    number.classList.toggle("selected");
 
-  number.addEventListener("click", (event) => {
-    for (let i = 0; i < ratingNumber.length; i++) {
-      if (ratingNumber[i].classList.contains("selected")) {
-        ratingNumber[i].classList.remove("selected");
+    number.addEventListener("click", () => {
+      for (let i = 0; i < ratingNumber.length; i++) {
+        if (ratingNumber[i].classList.contains("selected")) {
+          ratingNumber[i].classList.remove("selected");
+        }
       }
+      
+      selectedNumber = number.textContent;
+      numberError.style = "display: none";
+    })
+  });
+}
+
+function sendRating() {
+  button.addEventListener("click", () => {
+    if (selectedNumber) {
+      cardSelected.textContent = selectedNumber;
+      containerRating.style = "display: none";
+      containerThank.style = "display: flex";
+    } else {
+      numberError.style = "display: block";
     }
-    
-    number.classList.toggle("selected")
-    selectNumber = number.textContent;
   })
-});
+}
+
+
+
+selectNumber();
+sendRating();
